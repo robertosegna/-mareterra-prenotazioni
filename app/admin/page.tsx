e722ff8e8f118ee48f4ce83b076ce644f601b51a
roberto segna <robertsegna@yahoo.it>
2025-05-06 23:01:29 +0200

    Fix: creata pagina admin

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+  return (
+    <div>
+      <h1>Pagina Admin funzionante ✅</h1>
+      <p>Questa è la pagina di amministrazione appena deployata con successo.</p>
+    </div>
+  );
+}
